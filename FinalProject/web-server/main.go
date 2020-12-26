98149bffa61da691f50ee66ec081052dfbcb28c9
ci-runtime-serviceuser <eiriniintegrationteam@groups.sap.com>
2020-12-26 21:20:32 +0200

    Create poc look of the api for creation/deletion of user

--- FinalProject/web-server/main.go
+++ FinalProject/web-server/main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	"example.com/user/web-server/pkg/db/dao"
 	val "example.com/user/web-server/pkg/validator"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-type User struct {
+type RegistrationRequest struct {
+	Username string
+	Password string
+}
+
+type UserDTO struct {
+	gorm.Model
 	Username string
 	Password string
 }
@@ -19,28 +28,53 @@ type ErrorResponse struct {
 	ErrorMsg  string `json:"message"`
 }
 
+type RegistrationResponse struct {
+	Status int  `json:"status"`
+	ID     uint `json:"id"`
+}
+
 type SuccessResponse struct {
 	Status int `json:"status"`
 }
 
 var router = gin.Default()
+var uamDAO *dao.UamDAO
+
+func init() {
+	var err error
+	uamDAO, err = dao.NewUamDAO()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Cannot create DAO object. Reason: %v", err))
+		panic("Problem with DAO")
+	}
+
+	router = gin.Default()
+}
 
 func main() {
-	router.POST("/user/create", CreateUser)
+
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/user/register", createUser)
+		v1.POST("/user/delete/:id", deleteUser)
+	}
 	log.Fatal(router.Run(":8080"))
 }
 
-func CreateUser(c *gin.Context) {
-	var u User
-	if err := c.ShouldBindJSON(&u); err != nil {
+//Maybe the while validation procedure to be encapsualted in the registrationvalidator???
+func createUser(c *gin.Context) {
+	var rq RegistrationRequest
+
+	if err := c.ShouldBindJSON(&rq); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, ErrorResponse{
 			ErrorCode: http.StatusUnprocessableEntity,
 			ErrorMsg:  fmt.Sprintf("Cannot register user. Reason %v", err),
 		})
+		return
 	}
 
 	validator := val.NewValidator()
-	if err := validator.ValidateUsername(u.Username); err != nil {
+	if err := validator.ValidateUsername(rq.Username); err != nil {
 		c.JSON(http.StatusBadRequest, ErrorResponse{
 			ErrorCode: http.StatusBadRequest,
 			ErrorMsg:  fmt.Sprintf("Cannot register user. Reason %v", err),
@@ -48,15 +82,51 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	if err := validator.ValidatePassword(u.Password); err != nil {
+	if err := validator.ValidatePassword(rq.Password); err != nil {
 		c.JSON(http.StatusBadRequest, ErrorResponse{
 			ErrorCode: http.StatusBadRequest,
 			ErrorMsg:  fmt.Sprintf("Cannot register user. Reason %v", err),
+		})
+		return
+	}
+
+	userID, err := uamDAO.CreateUser(rq.Username, rq.Password)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{
+			ErrorCode: http.StatusInternalServerError,
+			ErrorMsg:  "Problem with the server, please try again later",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, RegistrationResponse{
+		Status: http.StatusCreated,
+		ID:     userID,
+	})
+}
+
+func deleteUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{
+			ErrorCode: http.StatusBadRequest,
+			ErrorMsg:  "Invalid type of id",
+		})
+		return
+	}
+
+	err = uamDAO.DeleteUser(uint(id))
+	if err != nil {
+		//if the error type is pariculary -> user not found
+		c.JSON(http.StatusInternalServerError, ErrorResponse{
+			ErrorCode: http.StatusBadRequest,
+			ErrorMsg:  "Problem with the deletion of the user",
 		})
 		return
 	}
 
 	c.JSON(http.StatusCreated, SuccessResponse{
-		Status: http.StatusCreated,
+		Status: http.StatusOK,
 	})
 }
