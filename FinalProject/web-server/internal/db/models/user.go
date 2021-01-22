4e781012cfed42d6ff3967762484f23fcaea83b1
i515142 <daniel.penchev@sap.com>
2021-01-23 01:03:45 +0200

    Change user model

--- FinalProject/web-server/internal/db/models/user.go
+++ FinalProject/web-server/internal/db/models/user.go
@@ -1,10 +1,12 @@
 package models
 
-import "gorm.io/gorm"
+import "time"
 
 //User is a model representing a record in the table of Users
 type User struct {
-	gorm.Model
-	Username string `gorm:"type:varchar(20);not null"`
-	Password string `gorm:"type:varchar(256);not null"`
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Username  string `gorm:"type:varchar(20);not null"`
+	Password  string `gorm:"type:varchar(256);not null"`
 }
