5d92cff68ba55c98999b1d75702c835118155ad4
i515142 <daniel.penchev@sap.com>
2021-01-23 01:08:27 +0200

    Add routees for the group management endpoints

--- FinalProject/web-server/main.go
+++ FinalProject/web-server/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	val "example.com/user/web-server/internal/validator"
 	"example.com/user/web-server/api/rest"
 	"example.com/user/web-server/internal/auth"
 	"example.com/user/web-server/internal/db/dao"
 	myerr "example.com/user/web-server/internal/error"
 	"example.com/user/web-server/internal/middleware"
+	val "example.com/user/web-server/internal/validator"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,17 +18,22 @@ func main() {
 	var (
 		uamDAO     dao.UamDAO
 		jwtCreator auth.JwtCreator
-		filter middleware.AuthzFilter
+		filter     middleware.AuthzFilter
 	)
 
 	uamDAO, err := dao.NewUamDAOImpl()
+	uamDAO.Migrate()
 	if err != nil {
-		log.Fatal(myerr.NewServerErrorWrap(err ,"Couldnt create a new User Access Management DAO"))
+		log.Fatal(myerr.NewServerErrorWrap(err, "Couldnt create a new User Access Management DAO"))
+	}
+
+	if err = uamDAO.Migrate(); err != nil {
+		log.Fatal(myerr.NewServerErrorWrap(err, "Couldnt migrate the database schemas"))
 	}
 
 	jwtCreator, err = auth.NewJwtCreatorImpl()
 	if err != nil {
-		log.Fatal(myerr.NewServerErrorWrap(err,"Couldnt create a new Jwt Creator"))
+		log.Fatal(myerr.NewServerErrorWrap(err, "Couldnt create a new Jwt Creator"))
 	}
 
 	filter = middleware.NewAuthzFilterImpl(jwtCreator)
@@ -45,6 +50,9 @@ func main() {
 
 		protected := v1.Group("/protected").Use(filter.Authz)
 		{
+			protected.POST("/group/membership/revocation", endpoint.RovokeMembership)
+			protected.POST("/group/creation", endpoint.CreateGroup)
+			protected.POST("/group/invitation", endpoint.AddMember)
 			protected.DELETE("/user/deletion", endpoint.DeleteUser)
 		}
 	}
